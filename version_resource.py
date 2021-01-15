e2c56db4e24225c1adf9a8d1331f63d8e02668c8
Mustafa <mustafa.khairullah@gmail.com>
2021-01-15 15:29:05 -0700

    Adding python script with Raw Github URL

--- version_resource.py
+++ version_resource.py
@@ -3,3 +3,7 @@ import requests
 print(requests.__version__)
 
 r = requests.get("http://google.com/")
+
+source = requests.get(
+    "https://raw.githubusercontent.com/mustafakhairullah/Lab1_404/master/version_resource.py")
+print(source.text)
